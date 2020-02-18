1f91be1de7c48e7e16f76b24b82b921d9ebdb1de
FernandoMX13 <luis.resendiz@bec.seguridad.unam.mx>
2020-02-18 17:42:49 -0600

    Login y menu

---
+++ Proyecto/Login.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Proyecto
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Usuario = new System.Windows.Forms.Label();
+            this.Contrasena = new System.Windows.Forms.Label();
+            this.ContrasenaBox = new System.Windows.Forms.TextBox();
+            this.UsuarioBox = new System.Windows.Forms.TextBox();
+            this.LoginBoton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Location = new System.Drawing.Point(20, 20);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(43, 13);
+            this.Usuario.TabIndex = 0;
+            this.Usuario.Text = "Usuario";
+            // 
+            // Contrasena
+            // 
+            this.Contrasena.AutoSize = true;
+            this.Contrasena.Location = new System.Drawing.Point(20, 55);
+            this.Contrasena.Name = "Contrasena";
+            this.Contrasena.Size = new System.Drawing.Size(61, 13);
+            this.Contrasena.TabIndex = 1;
+            this.Contrasena.Text = "Contraseña";
+            // 
+            // ContrasenaBox
+            // 
+            this.ContrasenaBox.Location = new System.Drawing.Point(100, 55);
+            this.ContrasenaBox.Name = "ContrasenaBox";
+            this.ContrasenaBox.Size = new System.Drawing.Size(360, 20);
+            this.ContrasenaBox.TabIndex = 2;
+            // 
+            // UsuarioBox
+            // 
+            this.UsuarioBox.Location = new System.Drawing.Point(100, 17);
+            this.UsuarioBox.Name = "UsuarioBox";
+            this.UsuarioBox.Size = new System.Drawing.Size(360, 20);
+            this.UsuarioBox.TabIndex = 3;
+            // 
+            // LoginBoton
+            // 
+            this.LoginBoton.Location = new System.Drawing.Point(220, 100);
+            this.LoginBoton.Name = "LoginBoton";
+            this.LoginBoton.Size = new System.Drawing.Size(75, 23);
+            this.LoginBoton.TabIndex = 4;
+            this.LoginBoton.Text = "Login";
+            this.LoginBoton.UseVisualStyleBackColor = true;
+            this.LoginBoton.Click += new System.EventHandler(this.LoginBoton_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(484, 136);
+            this.Controls.Add(this.LoginBoton);
+            this.Controls.Add(this.UsuarioBox);
+            this.Controls.Add(this.ContrasenaBox);
+            this.Controls.Add(this.Contrasena);
+            this.Controls.Add(this.Usuario);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 175);
+            this.MinimumSize = new System.Drawing.Size(500, 175);
+            this.Name = "Login";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.Label Contrasena;
+        private System.Windows.Forms.TextBox ContrasenaBox;
+        private System.Windows.Forms.TextBox UsuarioBox;
+        private System.Windows.Forms.Button LoginBoton;
+    }
+}
+
---
+++ Proyecto/Login.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Proyecto
+{
+    public partial class Login : Form
+    {
+        public static Dictionary<string, string> Users = new Dictionary<string, string>();
+        public static string getSHA256(string text)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+        public static void CrearUsuarios(string doc)
+        {
+            using (StreamWriter File = new StreamWriter(doc))
+            {
+                File.WriteLine("Dialid|de2d51a06c782f54b99a8c899506dd91004136653456fe8494000c9029243b59");
+                File.WriteLine("Mau|5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5");
+                File.WriteLine("Fer|65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5");
+            }
+        }
+        public Login()
+        {
+            string line;
+            string rutaDoc = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "BecarioMart");
+            string[] Usrpass = new string[2];
+            string doc = Path.Combine(rutaDoc, "usu");
+            DirectoryInfo dInfo = new DirectoryInfo(rutaDoc);
+            FileInfo passwd = new FileInfo(doc);
+            if (!dInfo.Exists)
+            {
+                dInfo.Create();
+            }
+            if (!passwd.Exists)
+            {
+                FileStream fs = passwd.Create();
+                fs.Close();
+                CrearUsuarios(doc);
+            }
+            StreamReader file = new StreamReader(doc);
+            while ((line = file.ReadLine()) != null)
+            {
+                Usrpass = line.Split('|');
+                Users.Add(Usrpass[0], Usrpass[1]);
+            }
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginBoton_Click(object sender, EventArgs e)
+        {
+            string checkUser = UsuarioBox.Text;
+            if (Users.ContainsKey(checkUser)) 
+            {
+                string checkPassword = getSHA256(ContrasenaBox.Text);
+                if (Users[checkUser] == checkPassword)
+                {
+                    MessageBox.Show("Acceso corecto.");
+                    Principal principal = new Principal();
+                    principal.Visible = true;
+                    this.Visible = false;
+                }
+                else
+                    MessageBox.Show("Contraseña incorrecta.");
+            }
+            else
+                MessageBox.Show("Usuario incorrecto.");
+
+        }
+    }
+}
