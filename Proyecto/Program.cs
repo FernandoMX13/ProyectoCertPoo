2e0fb4dfe1d059098aa4a889fb8e70cff842ff15
MauricioUrb <maurbina96@hotmail.com>
2020-02-18 18:56:45 -0600

    Prueba de manejador de git
    Prueba, sólo se agregó un comentario

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -17,6 +17,7 @@ namespace Proyecto
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
+            //
         }
     }
 }
